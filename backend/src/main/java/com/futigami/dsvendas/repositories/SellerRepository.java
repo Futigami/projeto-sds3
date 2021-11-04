85c705c77c6f63483fab3ddd6346fd5de66583cd
Futigami <erick.ertuito@hotmail.com>
2021-11-04 13:24:09 -0300

    Layers

---
+++ backend/src/main/java/com/futigami/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,11 @@
+package com.futigami.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.futigami.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+	
+	
+}
---
+++ backend/src/main/java/com/futigami/dsvendas/service/SellerService.java
@@ -0,0 +1,24 @@
+package com.futigami.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.futigami.dsvendas.dto.SellerDTO;
+import com.futigami.dsvendas.entities.Seller;
+import com.futigami.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+}
