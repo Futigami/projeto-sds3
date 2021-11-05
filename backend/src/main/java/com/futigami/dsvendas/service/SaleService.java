bb0630b6fb5d5677d99620b19ba54779030c9530
Futigami <erick.ertuito@hotmail.com>
2021-11-05 07:54:42 -0300

    Pagination

---
+++ backend/src/main/java/com/futigami/dsvendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.futigami.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.futigami.dsvendas.dto.SaleDTO;
+import com.futigami.dsvendas.entities.Sale;
+import com.futigami.dsvendas.repositories.SaleRepository;
+import com.futigami.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
